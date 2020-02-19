4a409a2cbceffa568d1139516471a652b2e8a233
Madeleine Johnson <mjohnson3038@gmail.com>
2020-02-18 19:47:57 -0500

    Improve test for bike lane availability

--- tests/test_bike_score.py
+++ tests/test_bike_score.py
@@ -1,14 +1,30 @@
+import pytest
+
+
 from bike_score_app import create_app
+from bike_score_app.bike_score import calculate_bike_lane_availability_score
+
 from flask import json
 
 TEST_REQUEST_CONTENT = {
    'bike_lane_availability': 80
 }
 
-
 def test_config():
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
+
+
+@pytest.mark.parametrize(('availability', 'expected'), (
+    (100, 100),
+    (0, 0),
+    (57, 57),
+    (33, 33),
+))
+def test_bike_lane_availability_score_returns_percentage_of_bike_lanes_available(availability, expected):
+    actual = calculate_bike_lane_availability_score(availability)
+    assert expected == actual
+
 
 def test_post_bike_score_returns_expected_json(client):
     response = client.post(
